c38bf8bda6f2a97f2e44c53ef8ed4e12a04f6e4a
Nathan Martin <63524975+nmartin42@users.noreply.github.com>
2022-08-08 16:52:19 -0400

    Correct typo

--- src/lib/hooks/useMediaBreakpoint.ts
+++ src/lib/hooks/useMediaBreakpoint.ts
@@ -1,7 +1,7 @@
 import { useMediaQuery } from 'react-responsive';
 
 /**
- * Check is a media query is valid.
+ * Check if a media query is valid.
  * @param query A string containing any integer followed by 'px'.
  * @returns A boolean. `true` if the current screen width matches the query. `false` if the current screen width does not match the query.
  */
